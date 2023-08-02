8cd31b4c44699d771439480c33acabd28e7ba682
Joel Perez <perezjoeldario@gmail.com>
2023-08-02 09:10:29 -0300

    se agrega try and catch

--- reenvioFuncionando.js
+++ reenvioFuncionando.js
@@ -5,124 +5,127 @@ const fs = require("fs");
 // Función para modificar el paquete recibido
 const newPackage = (data) => {
   // Aquí va tu lógica para procesar los datos recibidos
-
-  let divided = data.split(",");
-  let fechaHora = divided[6].split("");
-
-  function time(fechaHora) {
-    let hora = "";
-    for (let i = 6; i < fechaHora.length; i++) {
-      hora += fechaHora[i];
-    }
-    return hora;
-  }
-
-  let GPSStatus = divided[7];
-  let lat = divided[4];
-
-  const getACCStatusString = (paquete) => {
+  try {
     let divided = data.split(",");
-    let accStatus = divided[17];
+    let fechaHora = divided[6].split("");
 
-    if (accStatus === "0000") {
-      return "FFFFBBFF"; // ACC apagado
-    } else if (accStatus === "0400") {
-      return "FFFF9FFF"; // ACC encendido
-    } else {
-      return "Estado del ACC desconocido";
+    function time(fechaHora) {
+      let hora = "";
+      for (let i = 6; i < fechaHora.length; i++) {
+        hora += fechaHora[i];
+      }
+      return hora;
     }
-  };
 
-  function convertLatitude(lat) {
-    // Convertir la latitud a un número decimal.
-    lat = parseFloat(lat);
+    let GPSStatus = divided[7];
+    let lat = divided[4];
 
-    // Obtener los grados y minutos.
-    let degrees = Math.floor(Math.abs(lat));
-    let minutes = (Math.abs(lat) - degrees) * 60;
+    const getACCStatusString = (paquete) => {
+      let divided = data.split(",");
+      let accStatus = divided[17];
 
-    // Obtener la dirección (N o S).
-    let direction = lat >= 0 ? "N" : "S";
+      if (accStatus === "0000") {
+        return "FFFFBBFF"; // ACC apagado
+      } else if (accStatus === "0400") {
+        return "FFFF9FFF"; // ACC encendido
+      } else {
+        return "Estado del ACC desconocido";
+      }
+    };
 
-    // Devolver la latitud en el formato "DDMM.MMMM".
-    return `${degrees.toString().padStart(2, "0")}${minutes.toFixed(4)}`;
-  }
+    function convertLatitude(lat) {
+      // Convertir la latitud a un número decimal.
+      lat = parseFloat(lat);
 
-  let long = divided[5];
+      // Obtener los grados y minutos.
+      let degrees = Math.floor(Math.abs(lat));
+      let minutes = (Math.abs(lat) - degrees) * 60;
 
-  function convertLongitude(long) {
-    // Convertir la longitud a un número decimal.
-    long = parseFloat(long);
+      // Obtener la dirección (N o S).
+      let direction = lat >= 0 ? "N" : "S";
 
-    // Obtener los grados y minutos.
-    let degrees = Math.floor(Math.abs(long));
-    let minutes = (Math.abs(long) - degrees) * 60;
-
-    // Obtener la dirección (E o W).
-    let direction = long >= 0 ? "E" : "W";
-
-    // Devolver la longitud en el formato "DDDMM.MMMM".
-    return `${degrees.toString().padStart(3, "0")}${minutes.toFixed(4)}`;
-  }
-
-  let vel = divided[10];
-
-  function convertKmHToKnots(kmH) {
-    // Convertir km/h a nudos.
-    let knots = kmH / 1.852;
-
-    // Redondear a 4 decimales.
-    let roundedKnots = knots.toFixed(2);
-
-    // Asegurar que la velocidad tenga siempre 3 dígitos.
-    return roundedKnots.padStart(3, "0");
-  }
-
-  function date(fechaHora) {
-    let fecha = "";
-    for (let i = 0; i < 6; i++) {
-      fecha += fechaHora[i];
+      // Devolver la latitud en el formato "DDMM.MMMM".
+      return `${degrees.toString().padStart(2, "0")}${minutes.toFixed(4)}`;
     }
-    return (
-      fecha.slice(4) + fecha.slice(2, 4) + fecha.slice(0, 2)
-    ); /* Cambiar formato AAMMDD a DDMMAA */
+
+    let long = divided[5];
+
+    function convertLongitude(long) {
+      // Convertir la longitud a un número decimal.
+      long = parseFloat(long);
+
+      // Obtener los grados y minutos.
+      let degrees = Math.floor(Math.abs(long));
+      let minutes = (Math.abs(long) - degrees) * 60;
+
+      // Obtener la dirección (E o W).
+      let direction = long >= 0 ? "E" : "W";
+
+      // Devolver la longitud en el formato "DDDMM.MMMM".
+      return `${degrees.toString().padStart(3, "0")}${minutes.toFixed(4)}`;
+    }
+
+    let vel = divided[10];
+
+    function convertKmHToKnots(kmH) {
+      // Convertir km/h a nudos.
+      let knots = kmH / 1.852;
+
+      // Redondear a 4 decimales.
+      let roundedKnots = knots.toFixed(2);
+
+      // Asegurar que la velocidad tenga siempre 3 dígitos.
+      return roundedKnots.padStart(3, "0");
+    }
+
+    function date(fechaHora) {
+      let fecha = "";
+      for (let i = 0; i < 6; i++) {
+        fecha += fechaHora[i];
+      }
+      return (
+        fecha.slice(4) + fecha.slice(2, 4) + fecha.slice(0, 2)
+      ); /* Cambiar formato AAMMDD a DDMMAA */
+    }
+    let DireccionLat = lat >= 0 ? "N" : "S";
+    let DireccionLong = long >= 0 ? "E" : "W";
+    let Hora = time(fechaHora);
+    let Velocidad = convertKmHToKnots(vel);
+    let imei = "013226004207938";
+    let rumbo = divided[11];
+    let latitud = convertLatitude(Math.abs(lat)); // Quitamos el signo negativo
+    let longitud = convertLongitude(Math.abs(long)); // Quitamos el signo negativo
+    let fecha = date(fechaHora);
+
+    let accStatus = getACCStatusString(data); // Obtener el estado del ACC
+
+    let SendPackage = [
+      "*HQ",
+      imei,
+      "V1",
+      Hora,
+      GPSStatus,
+      latitud,
+      DireccionLat,
+      longitud,
+      DireccionLong,
+      Velocidad,
+      rumbo,
+      fecha,
+      accStatus, // Reemplazar "FFFFBBFF" por el estado del ACC
+      "722",
+      "310",
+      "06211",
+      "15036#",
+    ];
+
+    // Resto del código de la función newPackage...
+
+    // Devolver el paquete modificado en formato string
+    return SendPackage.join(",");
+  } catch (error) {
+    console.error(error);
   }
-  let DireccionLat = lat >= 0 ? "N" : "S";
-  let DireccionLong = long >= 0 ? "E" : "W";
-  let Hora = time(fechaHora);
-  let Velocidad = convertKmHToKnots(vel);
-  let imei = "013226004207938";
-  let rumbo = divided[11];
-  let latitud = convertLatitude(Math.abs(lat)); // Quitamos el signo negativo
-  let longitud = convertLongitude(Math.abs(long)); // Quitamos el signo negativo
-  let fecha = date(fechaHora);
-
-  let accStatus = getACCStatusString(data); // Obtener el estado del ACC
-
-  let SendPackage = [
-    "*HQ",
-    imei,
-    "V1",
-    Hora,
-    GPSStatus,
-    latitud,
-    DireccionLat,
-    longitud,
-    DireccionLong,
-    Velocidad,
-    rumbo,
-    fecha,
-    accStatus, // Reemplazar "FFFFBBFF" por el estado del ACC
-    "722",
-    "310",
-    "06211",
-    "15036#",
-  ];
-
-  // Resto del código de la función newPackage...
-
-  // Devolver el paquete modificado en formato string
-  return SendPackage.join(",");
 };
 
 // Función para enviar los datos a través de netcat
